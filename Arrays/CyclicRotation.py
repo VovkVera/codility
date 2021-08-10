f0aee2e5508ea75782641b944a7d0b53fb023153
Vera Vovk <vovkverait@gmail.com>
2021-08-10 19:20:57 +0300

    add first solution 62%

---
+++ Arrays/CyclicRotation.py
@@ -0,0 +1,14 @@
+def solution(A, K):
+    old = A
+    new = [0] * len(A)
+    for i in range(K):
+        new[0] = old[-1]
+        new[1:] = old[:-1]
+        old = new.copy()
+    return new
+
+
+if __name__ == '__main__':
+    A = [3, 8, 9, 7, 6]
+    K = 3
+    print(solution(A, K))
